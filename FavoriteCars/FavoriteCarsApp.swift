0134a27c6e08aefe65e71c94bd004e79349d849a
Emre Özcan <emre.ozcan@appcent.mobi>
2022-03-29 17:43:58 +0300

    Initial Commit

---
+++ FavoriteCars/FavoriteCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCarsApp.swift
+//  FavoriteCars
+//
+//  Created by Emre Özcan on 29.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
